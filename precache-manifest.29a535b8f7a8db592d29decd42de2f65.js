26343ad3ec87f4d72de8d70358b64696f9a1acd3
Pritesh Jha <priteshjha4u@gmail.com>
2019-02-21 15:41:05 +0530

    Updates

---
+++ precache-manifest.29a535b8f7a8db592d29decd42de2f65.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea20ba5748c557ad7290",
+    "url": "/react-redux-starter/static/js/4.ea20ba57.chunk.js"
+  },
+  {
+    "revision": "5a29d7d0f2fff6ec39f0",
+    "url": "/react-redux-starter/static/css/main.bb969fb5.chunk.css"
+  },
+  {
+    "revision": "e220031a47141521e7e2",
+    "url": "/react-redux-starter/static/js/runtime~main.e220031a.js"
+  },
+  {
+    "revision": "5b3b5966b7adad1391a64a3329559aa2",
+    "url": "/react-redux-starter/static/media/cat.5b3b5966.svg"
+  },
+  {
+    "revision": "19b24f1e78192fa2c76f",
+    "url": "/react-redux-starter/static/js/2.19b24f1e.chunk.js"
+  },
+  {
+    "revision": "de34eb7d90f82ccaeb36",
+    "url": "/react-redux-starter/static/js/3.de34eb7d.chunk.js"
+  },
+  {
+    "revision": "0c04f91b98ec8473ec9949ea6d5759e7",
+    "url": "/react-redux-starter/static/media/si-glyph-checked.0c04f91b.svg"
+  },
+  {
+    "revision": "5a29d7d0f2fff6ec39f0",
+    "url": "/react-redux-starter/static/js/main.5a29d7d0.chunk.js"
+  },
+  {
+    "revision": "f03bd8cc0a5eec76296b",
+    "url": "/react-redux-starter/static/js/5.f03bd8cc.chunk.js"
+  },
+  {
+    "revision": "b8736933f6973655b35f",
+    "url": "/react-redux-starter/static/js/6.b8736933.chunk.js"
+  },
+  {
+    "revision": "7f611cf5c13c8cfa18bd2e0422e0e9d5",
+    "url": "/react-redux-starter/static/media/si-glyph-edit.7f611cf5.svg"
+  },
+  {
+    "revision": "287cf2911e54d5793549b47e760dc7ba",
+    "url": "/react-redux-starter/static/media/si-glyph-delete.287cf291.svg"
+  },
+  {
+    "revision": "ea20ba5748c557ad7290",
+    "url": "/react-redux-starter/static/css/4.5168e765.chunk.css"
+  },
+  {
+    "revision": "19b24f1e78192fa2c76f",
+    "url": "/react-redux-starter/static/css/2.c1047d7d.chunk.css"
+  },
+  {
+    "revision": "c70d15ad967c7044f57b604a5b13f3ad",
+    "url": "/react-redux-starter/index.html"
+  }
+];
